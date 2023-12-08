57353533f30cc6fbe5469c02e835563cc9f6ef9d
Emma-Jonna <johanna.emma.pihl@gmail.com>
2023-12-08 08:50:45 +0100

    created logout button

---
+++ components/buttons/LogoutButton.tsx
@@ -0,0 +1,22 @@
+import {cookies} from "next/headers";
+import {redirect} from "next/navigation";
+import {createClient} from "@/utils/supabase/server";
+
+const LogOutButton = () => {
+  const signOut = async () => {
+    "use server";
+
+    const cookieStore = cookies();
+    const supabase = createClient(cookieStore);
+    await supabase.auth.signOut();
+    return redirect("/login");
+  };
+
+  return (
+    <form action={signOut}>
+      <button>Logout</button>
+    </form>
+  );
+};
+
+export default LogOutButton;
