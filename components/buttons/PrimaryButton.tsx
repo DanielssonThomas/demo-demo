e9a68638949a01b632f41d8c82bfca7142216095
Thomas Danielsson <69715206+DanielssonThomas@users.noreply.github.com>
2024-01-08 14:25:12 +0100

    feat: changed primary button and links

--- components/buttons/PrimaryButton.tsx
+++ components/buttons/PrimaryButton.tsx
@@ -2,14 +2,24 @@ const PrimaryButton = ({
   className,
   buttonText,
   onClick,
+  type,
 }: {
   className?: string;
   buttonText: string;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
+  type: "default" | "green" | "red" | "blue";
 }) => {
   return (
     <button
-      className={`bg-blue-500 px-4 py-2 border-2 border-solid border-gray-200 rounded-full ${className}`}
+      className={`${
+        type === "default"
+          ? "bg-light-bg dark:bg-dark-bg text-black dark:text-white"
+          : ""
+      } ${type === "green" ? "bg-btn-green text-white" : ""} ${
+        type === "red" ? "bg-btn-red text-white" : ""
+      } ${
+        type === "blue" ? "bg-btn-blue text-white" : ""
+      } px-4 py-2 border-[0.5px] border-solid border-black dark:border-white rounded-md inline-block ${className}`}
       onClick={onClick}
     >
       {buttonText}
--- components/links/PrimaryLink.tsx
+++ components/links/PrimaryLink.tsx
@@ -4,14 +4,24 @@ const PrimaryLink = ({
   className,
   linkText,
   href,
+  type,
 }: {
   className?: string;
   linkText: string;
   href: string;
+  type: "default" | "green" | "red";
 }) => {
   return (
     <Link className={`${className}`} href={href}>
-      <div className="bg-red-400 px-4 py-2 border-2 border-solid border-gray-200 rounded-full inline-block">
+      <div
+        className={`${
+          type === "default"
+            ? "bg-light-bg dark:bg-dark-bg text-black dark:text-white"
+            : ""
+        } ${type === "green" ? "bg-btn-green text-white" : ""} ${
+          type === "red" ? "bg-btn-red text-white" : ""
+        } px-4 py-2 border-[0.5px] border-solid border-black dark:border-white rounded-md inline-block`}
+      >
         <p>{linkText}</p>
       </div>
     </Link>
