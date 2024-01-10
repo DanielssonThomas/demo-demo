29b6ae268588c2ec5bf163fd2f684ef5028f580e
Emma-Jonna <johanna.emma.pihl@gmail.com>
2024-01-10 11:44:50 +0100

    updated displayed info for event table

--- app/(pages)/demonstrator/events/page.tsx
+++ app/(pages)/demonstrator/events/page.tsx
@@ -1,5 +1,22 @@
+"use client";
+
+import {useState, useEffect} from "react";
+import EventListItem from "@/components/cards/EventListItem";
+import DemonstratorEventInfoCard from "@/components/cards/DemonstratorEventInfoCard";
+import CloseButton from "@/components/buttons/CloseButton";
+import UnregisterButton from "@/components/buttons/UnregisterButton";
+
 const Events = () => {
-  return <div>this is the demonstrator events page</div>;
+  const [allEventsInfo, setAllEventsInfo] = useState<TableClientEvent[] | null>(null);
+  const [eventInfo, setEventInfo] = useState<TableClientEvent | null>(null);
+  const [eventInfoStyling, setEventInfoStyling] = useState("hidden");
+
+  return (
+    <div>
+      this is the demonstrator events page
+      {/* <DemonstratorEventInfoCard /> */}
+    </div>
+  );
 };
 
 export default Events;
